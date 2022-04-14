58f306dde3efd5d72b3f88ad8e85e7e815e468de
tungngovn <tung.ngothanh.hust@gmail.com>
2022-04-14 22:58:14 +0700

    Fedsdiv done, wait for upgrade

--- algorithm/mp_fedsdivv3.py
+++ algorithm/mp_fedsdivv3.py
@@ -58,7 +58,16 @@ def compute_similarity(a, b):
     sim = []
     for layer_a, layer_b in zip(a.parameters(), b.parameters()):
         x, y = torch.flatten(layer_a), torch.flatten(layer_b)
-        sim.append((x.T @ y) / (torch.norm(x) * torch.norm(y)))
+        if torch.isnan(x).any() or torch.isnan(y).any():
+            print("model contains nan")
+            exit(0)
+
+        norm_x, norm_y = torch.norm(x), torch.norm(y)
+        if norm_x == 0 or norm_y == 0:
+            sim.append(0.0)
+        else:
+            m = (x.T @ y) / (norm_x * norm_y)
+            sim.append(m)
 
     return torch.mean(torch.tensor(sim)), sim[-1]
 
@@ -66,35 +75,36 @@ def compute_similarity(a, b):
 class Server(MPBasicServer):
     def __init__(self, option, model, clients, test_data=None):
         super(Server, self).__init__(option, model, clients, test_data)
-        self.thr = 0.75
+        self.thr = 0.99
         
-        self.all_client_models = [copy.deepcopy(self.model) for _ in self.clients]
-        self.all_impact_factor = None
+        self.all_client_models = [copy.deepcopy(self.model).zeros_like() for _ in self.clients]
         self.last_all_impact_factor = [0 for _ in self.clients]
-        
+        self.optimal_ = np.array([1/6] * 6 + [1] * 4)
 
     def iterate(self, t, pool):
         self.selected_clients = self.sample()
+        print(self.selected_clients)
+        
         models, train_losses = self.communicate(self.selected_clients, pool)
         models = [model.to(torch.device(f"cuda:{self.server_gpu_id}")) for model in models]
         
         if not self.selected_clients:
             return
         
-        self.update_global_model(models, self.selected_clients)
-        
+        impact_factor = self.update_global_model(models, self.selected_clients, t)
+            
         # update model list
         self.update_all_client_models_list(models, self.selected_clients)
-        self.last_all_impact_factor = list.copy(self.all_impact_factor)
+        self.last_all_impact_factor = list.copy(impact_factor)
         return
 
 
     @torch.no_grad()
-    def update_global_model(self, model_list, idx_list):
+    def update_global_model(self, model_list, idx_list, t):
         device = torch.device(f"cuda:{self.server_gpu_id}")
         self.model = self.model.to(device)
-        new_models = []
         
+        new_models = []
         for model in model_list:
             for p, q in zip(model.parameters(), self.model.parameters()):
                 p = p - q
@@ -114,21 +124,49 @@ class Server(MPBasicServer):
         for i, model_i in zip(idx_list, new_models):
             for j, model_j in zip(idx_list, new_models):
                 similarity_matrix[i][j], _ = compute_similarity(model_i, model_j)
+                
+        min_s = torch.min(similarity_matrix[similarity_matrix > 0.0])
+        max_s = torch.max(similarity_matrix[similarity_matrix > 0.0])
+
+        if t % 3 == 0:
+            np.savetxt(f"algorithm/invest/v3/Q_matrix/Q_mtx_{t}.txt", similarity_matrix.numpy() * 1.0, fmt='%.5f', delimiter=',')
+            
+        similarity_matrix = (similarity_matrix - min_s)/(max_s - min_s) * (similarity_matrix > 0.0)
+
+        if torch.isnan(similarity_matrix).any():
+            print(similarity_matrix)
+            exit(0)
         
-        similarity_matrix = (similarity_matrix - torch.min(similarity_matrix))/(torch.max(similarity_matrix) - torch.min(similarity_matrix))
+        
         similarity_matrix = similarity_matrix > self.thr
         
+        if t % 3 == 0:
+            np.savetxt(f"algorithm/invest/v3/Cluster/Cluster_mtx_{t}.txt", similarity_matrix.numpy() * 1.0, fmt='%d', delimiter=',')
+            
+        # print("binary matrix:\n", similarity_matrix * 1.0)
         impact_factor = 1/torch.sum(similarity_matrix, dim=0)
+        impact_factor[torch.isinf(impact_factor)] = 0.0
+        impact_factor = torch.nan_to_num(impact_factor, 0.0)
+        
+        with open("algorithm/invest/v3/opt_loss.txt", "a+") as file:
+            loss = np.mean(np.power(impact_factor.numpy() - self.optimal_, 2))
+            file.write(f'{loss}\n')
+            
+        impact_factor = impact_factor / torch.sum(impact_factor)
+        impact_factor = impact_factor.detach().cpu().tolist()
+        
+        # print("impact now:", impact_factor)
+        # print("last impact:", self.last_all_impact_factor)
 
         for idx in range(len(self.all_client_models)):
             if idx in idx_list:
                 # If this client participate in this round's communication
-                self.model = self.model + impact_factor[idx] * model_list[idx_list.index(idx)] - self.all_impact_factor[idx] * self.all_client_models[idx]
+                self.model = self.model + impact_factor[idx] * model_list[idx_list.index(idx)] - self.last_all_impact_factor[idx] * self.all_client_models[idx]
             else:
                 # If not
-                self.model = self.model + (impact_factor[idx] - self.all_impact_factor[idx]) * self.all_client_models[idx]
-                
-        return
+                self.model = self.model + (impact_factor[idx] - self.last_all_impact_factor[idx]) * self.all_client_models[idx]
+        
+        return impact_factor
 
     
     def update_all_client_models_list(self, models, index):
@@ -171,6 +209,7 @@ class Client(MPBasicClient):
         tdata = self.data_to_device(data, device)    
         output_s, representation_s = model.pred_and_rep(tdata[0])                  # Student
         _ , representation_t = src_model.pred_and_rep(tdata[0])                    # Teacher
-        kl_loss = KL_divergence(representation_t, representation_s, device)        # KL divergence
+        # kl_loss = KL_divergence(representation_t, representation_s, device)        # KL divergence
+        kl_loss = 0.0
         loss = self.lossfunc(output_s, tdata[1])
         return loss, kl_loss
