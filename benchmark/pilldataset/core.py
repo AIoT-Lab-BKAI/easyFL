790d42cf1e2fd9612aedff6dfe7fa03aa2a46dab
sudo-u-Hung-NN <Hungnn184118@sis.hust.edu.vn>
2022-06-03 18:03:32 +0700

    debugged

--- benchmark/pilldataset/core.py
+++ benchmark/pilldataset/core.py
@@ -8,8 +8,8 @@ import json
 
 class TaskReader(BasicTaskReader):
     def __init__(self, taskpath="", data_folder="./dataset_idx/pill_dataset"):
-        self.data_folder = data_folder
         super(TaskReader,self).__init__(taskpath)
+        self.data_folder = data_folder
     
     def read_data(self):
         with open(f"{self.data_folder}/client_dataset/user_group_img.json",'r') as f:
@@ -23,9 +23,9 @@ class TaskReader(BasicTaskReader):
         with open(f"{self.data_folder}/server_dataset/img_label_dict.json",'r') as f:
             server_img_label_dict = json.load(f)  
         
-        test_dataset = PillDataset(0, "{self.data_folder}/server_dataset/pill_cropped",server_user_group_img,server_img_label_dict,label_hash)
+        test_dataset = PillDataset(0, f"{self.data_folder}/server_dataset/pill_cropped",server_user_group_img,server_img_label_dict,label_hash)
         n_clients = len(user_group_img)
-        train_data = [PillDataset(idx, "{self.data_folder}/client_dataset/pill_cropped", user_group_img, img_label_dict, label_hash) for idx in range(n_clients)]
+        train_data = [PillDataset(idx, f"{self.data_folder}/client_dataset/pill_cropped", user_group_img, img_label_dict, label_hash) for idx in range(n_clients)]
         
         return train_data,test_dataset,n_clients
 
