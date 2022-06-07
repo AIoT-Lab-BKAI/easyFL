3bdaa833962aba516e6e04328ec097e5be9946ce
sudo-u-Hung-NN <hung.nn184118@sis.hust.edu.vn>
2022-06-07 17:34:08 +0700

    vgg done

--- benchmark/cifar100/model/vgg.py
+++ benchmark/cifar100/model/vgg.py
@@ -10,10 +10,11 @@ cfg = {
     "E": [64, 64, "M", 128, 128, "M", 256, 256, 256, 256, "M", 512, 512, 512, 512, "M", 512, 512, 512, 512, "M"]
     }
 
+
 class Model(FModule):
     def __init__(self):
         super(Model, self).__init__()
-        self.features = make_layers(cfg["A"])
+        self.features = self._make_layers(cfg["A"])
         self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(512, 512),
@@ -42,67 +43,18 @@ class Model(FModule):
         o = self.classifier(e)
         return o, e
 
-
-def make_layers(cfg, batch_norm=False):
-    layers = []
-    in_channels = 3
-    for v in cfg:
-        if v == "M":
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+    def _make_layers(self, cfg, batch_norm=False):
+        layers = []
+        in_channels = 3
+        for v in cfg:
+            if v == "M":
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
             else:
-                layers += [conv2d, nn.ReLU(inplace=True)]
-            in_channels = v
-    
-    return nn.Sequential(*layers)
-
-
-
-# def vgg11_mnist(output_dim):
-#     """VGG 11-layer model (configuration "A")"""
-#     return VGG(make_layers_mnist(cfg["A"],output_dim))
-
-# def vgg11(output_dim):
-#     """VGG 11-layer model (configuration "A")"""
-#     return VGG(make_layers(cfg["A"],output_dim))
-
-# def cifar_vgg19():
-#     """VGG 19-layer model (configuration 'E')"""
-#     return VGG(make_layers(cfg["E"]))
-
-# def vgg11_bn():
-#     """VGG 11-layer model (configuration "A") with batch normalization"""
-#     return VGG(make_layers(cfg["A"], batch_norm=True))
-
-
-# def vgg13():
-#     """VGG 13-layer model (configuration "B")"""
-#     return VGG(make_layers(cfg["B"]))
-
-
-# def vgg13_bn():
-#     """VGG 13-layer model (configuration "B") with batch normalization"""
-#     return VGG(make_layers(cfg["B"], batch_norm=True))
-
-
-# def vgg16():
-#     """VGG 16-layer model (configuration "D")"""
-#     return VGG(make_layers(cfg["D"]))
-
-
-# def vgg16_bn():
-#     """VGG 16-layer model (configuration "D") with batch normalization"""
-#     return VGG(make_layers(cfg["D"], batch_norm=True))
-
-
-# def vgg19():
-#     """VGG 19-layer model (configuration "E")"""
-#     return VGG(make_layers(cfg["E"]))
-
-
-# def vgg19_bn():
-#     """VGG 19-layer model (configuration 'E') with batch normalization"""
-#     return VGG(make_layers(cfg["E"], batch_norm=True))
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                if batch_norm:
+                    layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                else:
+                    layers += [conv2d, nn.ReLU(inplace=True)]
+                in_channels = v
+        layers += [nn.Flatten(1,-1)]
+        return nn.Sequential(*layers)
