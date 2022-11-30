bee45e775c3ba49d271a5907c25b9d43ec4d6e12
thuydung-icthust <thuydungnguyen.ictk63@gmail.com>
2022-11-30 18:09:04 +0700

    Ready to mass running

---
+++ scripts/motiv2/gen_abci_scripts.py
@@ -0,0 +1,72 @@
+import os
+
+visible_cudas = [0, 1]
+cudas = ",".join([str(i) for i in visible_cudas])
+task_file = "main.py"
+
+dataset = "mnist"
+noniid = "pareto"
+N = 100
+K = 10
+total_epochs = 5000
+batch_size = 16
+
+model = "cnn"
+algos = ["scaffold", "mp_proposal", "mp_fedavg"]
+data_folder = f"./benchmark/{dataset}/data"
+log_folder = f"motiv/{dataset}"
+
+
+header_text = "\
+#!/bin/bash\n\
+#$ -cwd\n\
+#$ -l rt_G.small=1\n\
+#$ -l h_rt=36:00:00\n\
+#$ -o /home/aaa10078nj/Federated_Learning/Hung_test/logs/motiv2/$JOB_NAME_$JOB_ID.log\n\
+#$ -j y\n\n\
+source /etc/profile.d/modules.sh\n\
+module load gcc/11.2.0\n\
+module load openmpi/4.1.3\n\
+module load cuda/11.5/11.5.2\n\
+module load cudnn/8.3/8.3.3\n\
+module load nccl/2.11/2.11.4-1\n\
+module load python/3.10/3.10.4\n\
+source ~/venv/pytorch1.11+horovod/bin/activate\n\n\
+LOG_DIR=\"/home/aaa10078nj/Federated_Learning/Hung_test/logs/motiv2/mnist/$JOB_NAME_$JOB_ID\"\n\
+rm -r ${LOG_DIR}\n\
+mkdir ${LOG_DIR}\n\n\
+#Dataset\n\
+DATA_DIR=\"$SGE_LOCALDIR/$JOB_ID/\"\n\
+cp -r ./easyFL/benchmark/mnist/data ${DATA_DIR}\n\n\
+"
+
+formated_command = "\
+GROUP=\"{}\"\n\
+ALG=\"{}\"\n\
+MODEL=\"{}\"\n\
+WANDB=1\n\
+ROUND={}\n\
+EPOCH_PER_ROUND={}\n\
+BATCH={}\n\
+PROPOTION={:>.2f}\n\
+NUM_THRESH_PER_GPU=1\n\
+NUM_GPUS=1\n\
+SERVER_GPU_ID=0\n\
+TASK=\"{}\"\n\
+DATA_IDX_FILE=\"mnist/100client/pareto/MNIST-noniid-pareto_1.json\"\n\n\
+cd easyFL\n\n\
+"
+
+for E in [1, 5, 10, 20, 25, 40, 50]:
+    task_name = f"{dataset}_{noniid}_N{N}_K{K}"
+
+    for algo in algos:
+        command = formated_command.format(
+            task_name, algo, model, int(total_epochs/E), E, batch_size, K/N, task_name
+        )
+            
+        body_text = "python main.py  --task ${TASK}  --model --model ${MODEL}  --algorithm ${ALG}  --wandb ${WANDB} --data_folder ${DATA_DIR}  --log_folder ${LOG_DIR}   --dataidx_filename ${DATA_IDX_FILE}   --num_rounds ${ROUND} --num_epochs ${EPOCH_PER_ROUND} --proportion ${PROPOTION} --batch_size ${BATCH} --num_threads_per_gpu ${NUM_THRESH_PER_GPU}  --num_gpus ${NUM_GPUS} --server_gpu_id ${SERVER_GPU_ID} "
+
+        file = open(f"./{task_name}_E{E}_{algo}.sh", "w")
+        file.write(header_text + command + body_text)
+        file.close()
--- scripts/motiv2/mnist_pareto_N100_K10_E1_mp_proposal.sh
+++ scripts/motiv2/mnist_pareto_N100_K10_E1_mp_proposal.sh
@@ -1,1 +1,41 @@
-CUDA_VISIBLE_DEVICES=0,1 python main.py  --task mnist_pareto_N100_K10  --model cnn  --algorithm mp_proposal  --wandb 1  --data_folder "./benchmark/mnist/data"  --log_folder "motiv/mnist"  --dataidx_filename "mnist/100client/pareto/MNIST-noniid-pareto_1.json"  --num_rounds 5000  --num_epochs 1  --proportion 0.1  --batch_size 16  --num_threads_per_gpu 1   --num_gpus 2  --server_gpu_id 0
+#!/bin/bash
+#$ -cwd
+#$ -l rt_G.small=1
+#$ -l h_rt=36:00:00
+#$ -o /home/aaa10078nj/Federated_Learning/Hung_test/logs/motiv2/$JOB_NAME_$JOB_ID.log
+#$ -j y
+
+source /etc/profile.d/modules.sh
+module load gcc/11.2.0
+module load openmpi/4.1.3
+module load cuda/11.5/11.5.2
+module load cudnn/8.3/8.3.3
+module load nccl/2.11/2.11.4-1
+module load python/3.10/3.10.4
+source ~/venv/pytorch1.11+horovod/bin/activate
+
+LOG_DIR="/home/aaa10078nj/Federated_Learning/Hung_test/logs/motiv2/mnist/$JOB_NAME_$JOB_ID"
+rm -r ${LOG_DIR}
+mkdir ${LOG_DIR}
+
+#Dataset
+DATA_DIR="$SGE_LOCALDIR/$JOB_ID/"
+cp -r ./easyFL/benchmark/mnist/data ${DATA_DIR}
+
+GROUP="mnist_pareto_N100_K10"
+ALG="mp_proposal"
+MODEL="cnn"
+WANDB=1
+ROUND=5000
+EPOCH_PER_ROUND=1
+BATCH=16
+PROPOTION=0.10
+NUM_THRESH_PER_GPU=1
+NUM_GPUS=1
+SERVER_GPU_ID=0
+TASK="mnist_pareto_N100_K10"
+DATA_IDX_FILE="mnist/100client/pareto/MNIST-noniid-pareto_1.json"
+
+cd easyFL
+
+python main.py  --task ${TASK}  --model --model ${MODEL}  --algorithm ${ALG}  --wandb ${WANDB} --data_folder ${DATA_DIR}  --log_folder ${LOG_DIR}   --dataidx_filename ${DATA_IDX_FILE}   --num_rounds ${ROUND} --num_epochs ${EPOCH_PER_ROUND} --proportion ${PROPOTION} --batch_size ${BATCH} --num_threads_per_gpu ${NUM_THRESH_PER_GPU}  --num_gpus ${NUM_GPUS} --server_gpu_id ${SERVER_GPU_ID} 
