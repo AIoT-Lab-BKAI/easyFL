4907cea11325d7d065ff1cce70b571e0cad2e0bc
thuydung-icthust <thuydungnguyen.ictk63@gmail.com>
2022-11-30 23:38:39 +0700

    Prepare for emnist

--- utils/gendata/gen.py
+++ utils/gendata/gen.py
@@ -1,6 +1,6 @@
-from noniid_featured import cifar_noniid_featured, cifar100_noniid_featured
-from noniid_pareto import pareto_noniid, cifar100_noniid_pareto
-from noniid_quantitative import cifar_noniid_quantitative, cifar100_noniid_quantitative
+from noniid_featured import noniid_featured
+from noniid_pareto import noniid_pareto
+from noniid_quantitative import noniid_quantitative
 from torchvision import datasets
 from torchvision import datasets, transforms
 import json
@@ -38,8 +38,10 @@ def gen_full(dataset,total_client, num_sample=50000):
 
 
 if __name__ == '__main__':
-    train_dataset = datasets.CIFAR10( "../../benchmark/cifar10/data/", train=True, download=False, transform=None)
+    # train_dataset = datasets.CIFAR10("../../benchmark/cifar10/data/", train=True, download=False, transform=None)
+    train_dataset = datasets.EMNIST("../../benchmark/emnist/data/", split='letters', train=True, download=True, transform=None)
     print("total sample of dataset", len(train_dataset))
+    
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--method", choices=['featured', 'quantitative', 'pareto'])
@@ -47,18 +49,18 @@ if __name__ == '__main__':
     args = parser.parse_args()
     
     if args.method == 'featured':
-        client_dict = cifar100_noniid_featured(train_dataset, total_client=args.total_client, total_label=10)
+        client_dict = noniid_featured(train_dataset, total_client=args.total_client, total_label=37)
     elif args.method == 'quantitative':
-        client_dict = cifar100_noniid_quantitative(train_dataset, total_client=args.total_client, total_label=10)
+        client_dict = noniid_quantitative(train_dataset, total_client=args.total_client, total_label=37)
     elif args.method == 'pareto':
-        client_dict = cifar100_noniid_pareto(train_dataset, total_client=args.total_client, total_label=10)
+        client_dict = noniid_pareto(train_dataset, total_client=args.total_client, total_label=37)
     else:
         raise Exception("Not recognise data distributing method")
     
     print("Gen done!")
-    save_dataset_idx(client_dict, f"../../dataset_idx/cifar10/{args.total_client}client/", f"CIFAR10_{args.total_client}client_{args.method}.json")
+    save_dataset_idx(client_dict, f"../../dataset_idx/emnist/{args.total_client}client/", f"emnist_{args.total_client}client_{args.method}.json")
     df = sta(client_dict, train_dataset, num_client=args.total_client, num_label=10)
     print("Total sample in modified dataset", df.values.sum())
-    df.to_csv(f"../../dataset_idx/cifar10/{args.total_client}client/CIFAR10_{args.total_client}client_{args.method}.csv", index=False, header=False)
+    df.to_csv(f"../../dataset_idx/emnist/{args.total_client}client/emnist_{args.total_client}client_{args.method}.csv", index=False, header=False)
   
     
--- utils/gendata/noniid_pareto.py
+++ utils/gendata/noniid_pareto.py
@@ -5,79 +5,8 @@ import math
 from torchvision import datasets, transforms
 import json
 
-def pareto_noniid(dataset, total_client, total_sample=50000, total_label=10):
-    labels = dataset.targets
-    idxs = range(len(dataset))
-    idxs_labels = np.vstack((idxs, labels))
-    idxs_labels = idxs_labels[:, idxs_labels[1, :].argsort()]
-    idxs = idxs_labels[0, :]
-    labels = idxs_labels[1, :]
-    tmp = 0
-    list_tmp = []
 
-    for i in range(len(dataset)):
-        if(dataset[idxs[i]][1] == tmp):
-            tmp +=1
-            list_tmp.append(i)
-    list_tmp.append(len(dataset))
-      
-    while(True):
-        list_label ={}
-        a = set()
-        for i in range(total_client):
-            list_label[i] = np.random.randint(0,total_label,3) 
-            a.update(list_label[i])
-        print(len(a))
-        if len(a) >= total_label:
-            break
-    
-    key  = True
-    count = 0
-    while(key):
-        count += 1
-        if count > 100:
-            print("Infinite loop")
-            exit(0)
-            
-        try:
-            list_dict = [0] * total_label
-            for i in range(total_label):
-                list_dict[i] = idxs[list_tmp[i]:list_tmp[i+1]]
-
-            dis = np.random.pareto(tuple([1] * total_client))
-            dis = dis/np.sum(dis)
-            percent = [0] * 100
-            for i in range(total_client):
-                for j in list_label[i]:
-                    percent[j] += dis[i]
-
-            maxx = max(percent)
-            total = np.around(2000/maxx)
-            sample_client = [math.ceil(total * dis[i]) for i in range(total_client)]
-            for i in  range(len(sample_client)):
-                if sample_client[i] == 0:
-                    sample_client[i] = 1
-                
-            dict_client = {}
-            for i in range(total_client):
-                dict_client[i] = []
-            for i in range(total_client):
-
-                x = math.ceil(sample_client[i]/2)
-                for j in list_label[i]:
-                    a = np.random.choice(list_dict[j],x,replace=False)
-                    list_dict[list_label[i][0]] = list(set(list_dict[list_label[i][0]]) - set(a))
-                    dict_client[i] = dict_client[i]  + list(a)
-
-                dict_client[i] = [int(j) for j in dict_client[i]]
-            key = False
-        except:
-            key = True
-        
-    return dict_client
-
-
-def cifar100_noniid_pareto(dataset, total_client, total_label=100):
+def noniid_pareto(dataset, total_client, total_label=100):
     total_label = total_label
     labels = dataset.targets
     idxs = range(len(dataset))
