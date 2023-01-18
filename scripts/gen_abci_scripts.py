2b1be5f2bef1b2addb4c84c393939f701e1204f0
duclong1009 <duclong10092000@gmail.com>
2023-01-18 17:26:09 +0700

    add scripts

--- scripts/gen_abci_scripts.py
+++ scripts/gen_abci_scripts.py
@@ -1,26 +1,27 @@
 import os
 from pathlib import Path
 
-dataset = "mnist"
+dataset = "cifar10"
 noniid = "dir_1_sparse"
 N = 20
 K = 5
-total_epochs = 2000
+total_epochs = 4000
 batch_size = 16
 
-model = "cnn"
-algos = ["scaffold", "mp_fedavg", "mp_fedprox", "mp_proposal"]
+models = ["cnn", 'mlp']
+for model in models:
+    algos = ["scaffold", "mp_fedavg", "mp_fedprox", "mp_proposal"]
 
-if not Path(f"./{dataset}/{N}_clients").exists():
-    os.makedirs(f"./{dataset}/{N}_clients")
+    if not Path(f"./{dataset}/{model}/{N}_clients").exists():
+        os.makedirs(f"./{dataset}/{model}/{N}_clients")
 
 
-header_text = "\
+    header_text = "\
 #!/bin/bash\n\
 #$ -cwd\n\
 #$ -l rt_G.small=1\n\
 #$ -l h_rt=36:00:00\n\
-#$ -o /home/aaa10078nj/Federated_Learning/Hung_perFL/logs/mnist/$JOB_NAME_$JOB_ID.log\n\
+#$ -o /home/aaa10078nj/Federated_Learning/Hung_perFL/logs/cifar10/$JOB_NAME_$JOB_ID.log\n\
 #$ -j y\n\n\
 source /etc/profile.d/modules.sh\n\
 module load gcc/11.2.0\n\
@@ -30,15 +31,15 @@ module load cudnn/8.3/8.3.3\n\
 module load nccl/2.11/2.11.4-1\n\
 module load python/3.10/3.10.4\n\
 source ~/venv/pytorch1.11+horovod/bin/activate\n\n\
-LOG_DIR=\"/home/aaa10078nj/Federated_Learning/Hung_perFL/logs/mnist/$JOB_NAME_$JOB_ID\"\n\
+LOG_DIR=\"/home/aaa10078nj/Federated_Learning/Hung_perFL/logs/cifar10/$JOB_NAME_$JOB_ID\"\n\
 rm -r ${LOG_DIR}\n\
 mkdir ${LOG_DIR}\n\n\
 #Dataset\n\
 DATA_DIR=\"$SGE_LOCALDIR/$JOB_ID/\"\n\
-cp -r ./easyFL/benchmark/mnist/data ${DATA_DIR}\n\n\
+cp -r ./easyFL/benchmark/cifar10/data ${DATA_DIR}\n\n\
 "
 
-formated_command = "\
+    formated_command = "\
 GROUP=\"{}\"\n\
 ALG=\"{}\"\n\
 MODEL=\"{}\"\n\
@@ -55,7 +56,7 @@ IDX_DIR=\"{}/dirichlet/{}/{}client\"\n\n\
 cd easyFL\n\n\
 "
 
-body_text = "\
+    body_text = "\
 python main.py \
 --task ${TASK} \
 --model ${MODEL} \
@@ -74,14 +75,14 @@ python main.py \
 --server_gpu_id ${SERVER_GPU_ID}"
 
 
-for E in [1, 8, 16, 32]:
-    task_name = f"{dataset}_{noniid}_N{N}_K{K}_E{E}"
+    for E in [1, 8, 16]:
+        task_name = f"{dataset}_{model}_{noniid}_N{N}_K{K}_E{E}"
 
-    for algo in algos:
-        command = formated_command.format(
-            task_name, algo, model, max(300, int(total_epochs/E)), E, batch_size, K/N, 1, task_name, dataset, noniid, N
-        )
-        
-        file = open(f"./{dataset}/{N}_clients/{task_name}_{algo}.sh", "w")
-        file.write(header_text + command + body_text)
-        file.close()
+        for algo in algos:
+            command = formated_command.format(
+                task_name, algo, model, max(300, int(total_epochs/E)), E, batch_size, K/N, 1, task_name, dataset, noniid, N
+            )
+            
+            file = open(f"./{dataset}/{model}/{N}_clients/{task_name}_{algo}.sh", "w")
+            file.write(header_text + command + body_text)
+            file.close()
