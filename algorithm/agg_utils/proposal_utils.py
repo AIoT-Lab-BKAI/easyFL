560ded05478f717d4191324e59f2cca834eb0cd5
bangdabezt <bangdabezt@gmail.com>
2023-05-16 10:32:12 +0700

    Journal version uploaded

---
+++ algorithm/agg_utils/proposal_utils.py
@@ -0,0 +1,133 @@
+import math
+import random
+
+import gym
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.normal_(m.weight, mean=0., std=0.1)
+        nn.init.constant_(m.bias, 0.1)
+        
+        
+def compute_gae(next_value, rewards, masks, values, gamma=0.99, tau=0.95):
+    values = values + [next_value]
+    gae = 0
+    returns = []
+    for step in reversed(range(len(rewards))):
+        delta = rewards[step] + gamma * values[step + 1] * masks[step] - values[step]
+        gae = delta + gamma * tau * masks[step] * gae
+        returns.insert(0, gae + values[step])
+    return returns
+
+
+def ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantage):
+    batch_size = states.size(0)
+    for _ in range(batch_size // mini_batch_size):
+        rand_ids = np.random.randint(0, batch_size, mini_batch_size)
+        yield states[rand_ids, :], actions[rand_ids, :], log_probs[rand_ids, :], returns[rand_ids, :], advantage[rand_ids, :]
+         
+
+def ppo_update(model, optimizer, ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantages, clip_param=0.2):
+    for _ in range(ppo_epochs):
+        for state, action, old_log_probs, return_, advantage in ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantages):
+            dist, value = model(state)
+            entropy = dist.entropy().mean()
+            new_log_probs = dist.log_prob(action)
+
+            ratio = (new_log_probs - old_log_probs).exp()
+            surr1 = ratio * advantage
+            surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) * advantage
+
+            actor_loss  = - torch.min(surr1, surr2).mean()
+            critic_loss = (return_ - value).pow(2).mean()
+
+            loss = 0.5 * critic_loss + actor_loss - 0.001 * entropy
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0):
+        super(ActorCritic, self).__init__()
+        
+        self.critic = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+        
+        self.actor = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_outputs),
+        )
+        self.log_std = nn.Parameter(torch.ones(1, num_outputs) * std)
+        
+        self.apply(init_weights)
+        self.init_rl()
+        return
+    
+    def init_rl(self):
+        self.log_probs = []
+        self.values    = []
+        self.states    = []
+        self.actions   = []
+        self.rewards   = []
+        self.masks     = []
+        self.entropy   = 0
+        return
+        
+    def forward(self, x):
+        value = self.critic(x)
+        mu    = self.actor(x)
+        std   = self.log_std.exp().expand_as(mu)
+        dist  = Normal(mu, std)
+        return dist, value
+    
+    def get_action(self, state):
+        dist, value = self(state)
+        action = dist.sample()
+        
+        log_prob = dist.log_prob(action)
+        self.entropy += dist.entropy().mean()
+        
+        self.log_probs.append(log_prob)
+        self.values.append(value)
+        self.states.append(state)
+        self.actions.append(action)
+        return action
+    
+    def record(self, reward, done=0):
+        self.rewards.append(torch.FloatTensor(reward).unsqueeze(1))
+        self.masks.append(torch.FloatTensor(1 - done).unsqueeze(1))
+        return
+    
+    def update(self, next_state, optimizer, ppo_epochs=4, mini_batch_size=5):
+        next_state = torch.FloatTensor(next_state)
+        _, next_value = self(next_state)
+        returns = compute_gae(next_value, self.rewards, self.masks, self.values)
+
+        returns   = torch.cat(returns).detach()
+        log_probs = torch.cat(self.log_probs).detach()
+        values    = torch.cat(self.values).detach()
+        states    = torch.cat(self.states)
+        actions   = torch.cat(self.actions)
+        advantage = returns - values
+        
+        ppo_update(self, optimizer, ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantage)
+        self.init_rl()
+        return
---
+++ algorithm/proposal.py
@@ -0,0 +1,153 @@
+from .mp_fedbase import MPBasicServer, MPBasicClient
+from algorithm.cfmtx.cfmtx import cfmtx_test
+from algorithm.agg_utils.proposal_utils import ActorCritic
+
+import torch.nn as nn
+import numpy as np
+import torch
+import copy
+
+
+def get_module_from_model(model, res = None):
+    if res==None: res = []
+    ch_names = [item[0] for item in model.named_children()]
+    if ch_names==[]:
+        if model._parameters:
+            res.append(model)
+    else:
+        for name in ch_names:
+            get_module_from_model(model.__getattr__(name), res)
+    return res
+
+
+def get_classifier(model):
+    modules = get_module_from_model(model)
+    penul = modules[-1]._parameters['weight']
+    return penul
+
+
+def KL_divergence(teacher_batch_input, student_batch_input, device):
+    """
+    Compute the KL divergence of 2 batches of layers
+    Args:
+        teacher_batch_input: Size N x d
+        student_batch_input: Size N x c
+    
+    Method: Kernel Density Estimation (KDE)
+    Kernel: Gaussian
+    Author: Nguyen Nang Hung
+    """
+    batch_student, _ = student_batch_input.shape
+    batch_teacher, _ = teacher_batch_input.shape
+    
+    assert batch_teacher == batch_student, "Unmatched batch size"
+    
+    teacher_batch_input = teacher_batch_input.to(device).unsqueeze(1)
+    student_batch_input = student_batch_input.to(device).unsqueeze(1)
+    
+    sub_s = student_batch_input - student_batch_input.transpose(0,1)
+    sub_s_norm = torch.norm(sub_s, dim=2)
+    sub_s_norm = sub_s_norm.flatten()[1:].view(batch_student-1, batch_student+1)[:,:-1].reshape(batch_student, batch_student-1)
+    std_s = torch.std(sub_s_norm)
+    mean_s = torch.mean(sub_s_norm)
+    kernel_mtx_s = torch.pow(sub_s_norm - mean_s, 2) / (torch.pow(std_s, 2) + 0.001)
+    kernel_mtx_s = torch.exp(-1/2 * kernel_mtx_s)
+    kernel_mtx_s = kernel_mtx_s/torch.sum(kernel_mtx_s, dim=1, keepdim=True)
+    
+    sub_t = teacher_batch_input - teacher_batch_input.transpose(0,1)
+    sub_t_norm = torch.norm(sub_t, dim=2)
+    sub_t_norm = sub_t_norm.flatten()[1:].view(batch_teacher-1, batch_teacher+1)[:,:-1].reshape(batch_teacher, batch_teacher-1)
+    std_t = torch.std(sub_t_norm)
+    mean_t = torch.mean(sub_t_norm)
+    kernel_mtx_t = torch.pow(sub_t_norm - mean_t, 2) / (torch.pow(std_t, 2) + 0.001)
+    kernel_mtx_t = torch.exp(-1/2 * kernel_mtx_t)
+    kernel_mtx_t = kernel_mtx_t/torch.sum(kernel_mtx_t, dim=1, keepdim=True)
+    
+    kl = torch.sum(kernel_mtx_t * torch.log(kernel_mtx_t/kernel_mtx_s))
+    return kl
+
+
+def compute_similarity(a, b):
+    """
+    Parameters:
+        a, b [torch.nn.Module]
+    Returns:
+        sum of pair-wise similarity between layers of a and b
+    """
+    sim = []
+    for layer_a, layer_b in zip(a.parameters(), b.parameters()):
+        x, y = torch.flatten(layer_a), torch.flatten(layer_b)
+        sim.append((x.transpose(-1,0) @ y) / (torch.norm(x) * torch.norm(y)))
+
+    return torch.mean(torch.tensor(sim)), sim[-1]
+
+
+class Server(MPBasicServer):
+    def __init__(self, option, model, clients, test_data=None):
+        super(Server, self).__init__(option, model, clients, test_data)
+        classifier_length = get_classifier(model).flatten().shape[0]
+        self.agent = ActorCritic(num_inputs=self.clients_per_round * classifier_length, num_outputs=self.clients_per_round, hidden_size=512)
+        return
+    
+    def iterate(self, t, pool):
+        self.selected_clients = self.sample()
+        models, train_losses = self.communicate(self.selected_clients, pool)
+        
+        if not self.selected_clients: 
+            return
+        
+        # Get classifiers
+        classifiers = [get_classifier(model).cpu().flatten() for model in models]
+        state = torch.vstack(classifiers)         # <-- Change to matrix K x d
+        
+        # Processing
+        if t > 0:
+            reward = - np.mean(train_losses)
+            self.agent.record(reward)
+        impact_factors = self.agent.get_action(state)
+        
+        device0 = torch.device(f"cuda:{self.server_gpu_id}")
+        models = [i.to(device0) for i in models]
+        self.model = self.aggregate(models, p = impact_factors)
+        return
+
+
+class Client(MPBasicClient):
+    def __init__(self, option, name='', train_data=None, valid_data=None):
+        super(Client, self).__init__(option, name, train_data, valid_data)
+        self.lossfunc = nn.CrossEntropyLoss()
+        self.kd_fct = option['kd_fct']
+        
+    def reply(self, svr_pkg, device):
+        model = self.unpack(svr_pkg)
+        loss = self.train_loss(model, device)
+        self.train(model, device)
+        cpkg = self.pack(model, loss)
+        return cpkg
+        
+    def train(self, model, device='cuda'):
+        model = model.to(device)
+        model.train()
+        
+        src_model = copy.deepcopy(model).to(device)
+        src_model.freeze_grad()
+                
+        data_loader = self.calculator.get_data_loader(self.train_data, batch_size=self.batch_size, droplast=True)
+        optimizer = self.calculator.get_optimizer(self.optimizer_name, model, lr=self.learning_rate, weight_decay=self.weight_decay, momentum=self.momentum)
+        
+        for iter in range(self.epochs):
+            for batch_id, batch_data in enumerate(data_loader):
+                model.zero_grad()
+                loss, kl_loss = self.get_loss(model, src_model, batch_data, device)
+                loss = loss + self.kd_fct * kl_loss
+                loss.backward()
+                optimizer.step()
+        return
+
+    def get_loss(self, model, src_model, data, device):
+        tdata = self.calculator.data_to_device(data, device)    
+        output_s, representation_s = model.pred_and_rep(tdata[0])                  # Student
+        _ , representation_t = src_model.pred_and_rep(tdata[0])                    # Teacher
+        kl_loss = KL_divergence(representation_t, representation_s, device)        # KL divergence
+        loss = self.lossfunc(output_s, tdata[1])
+        return loss, kl_loss
